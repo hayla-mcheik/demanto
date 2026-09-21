cb5c07f27fb5738963e616f82d9662e458b05932
hayla-mcheik <117450554+hayla-mcheik@users.noreply.github.com>
2026-09-21 21:38:10 +0300

    changes

--- resources/views/frontend/collections/products/view.blade.php
+++ resources/views/frontend/collections/products/view.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', $product->name . ' | DEMANTO Jewelry')
+@section('title', $product->name . ' | DEMANTO')
 {{ $product->meta_title }}
 @endsection
 
@@ -8,9 +8,7 @@
 @endsection
 
 @section('meta_description')
-<meta name="description"
-      content="{{ Str::limit(strip_tags($product->description), 155) }}">
-{{-- {{ $product->meta_description }} --}}
+{{ $product->meta_description }}
 @endsection
 
 @section('content')
